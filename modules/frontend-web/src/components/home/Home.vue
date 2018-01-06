a27cc525868c095a9c9452efd5747150cd11467d
takashno <nozomu.takashima@gmail.com>
2018-01-06 14:19:31 +0900

    - SwaggerのYAMLを少し更新 - Model設計を進めた - 全般的に名称を変更した

---
+++ modules/frontend-web/src/components/home/Home.vue
@@ -0,0 +1,120 @@
+<template>
+<main class="col-md-10" style="padding:0px;border-left:1px solid gray;">
+  <div class="row">
+      <div class="col-md-12">
+          <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                  <li class="breadcrumb-item active" aria-current="page">HOME</li>
+              </ol>
+          </nav>
+      </div>
+  </div>
+  <article class="row" style="padding: 30px 10px 30px 10px;">
+    <section class="col-md-12">
+        <h4>New Learning Program</h4>
+        <div class="row">
+            <div class="col">
+                <table class="table table-sm table-striped">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Tags</th>
+                    </tr>
+
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><a href="#">ラムダ式</a></td>
+                        <td>プログラミング</td>
+                        <td>Java,Java8,Stream</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">サーバーレスアーキテクチャ</a></td>
+                        <td>システム</td>
+                        <td>AWS,Lambda,APIGateway</td>
+                    </tr>
+                    <tr>
+                        <td><a href="contents001.html">マイクロサービスアーキテクチャ</a></td>
+                        <td>システム</td>
+                        <td>REST,MSA,SpringBoot</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">DIコンテナ</a></td>
+                        <td>フレームワーク</td>
+                        <td>SpringFramework,DI</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">GCアルゴリズム</a></td>
+                        <td>プログラミング</td>
+                        <td>GC,Java,G1GC,CSMC</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <a class="btn btn-info" href="#" role="button">More...</a>
+            </div>
+        </div>
+    </section>
+    <section class="col-md-12" style="margin-top:30px;">
+        <h4>Popular Program</h4>
+        <div class="row">
+            <div class="col">
+                <table class="table table-sm table-striped">
+                    <thead>
+                    <tr>
+                        <th>プログラム</th>
+                        <th>ジャンル</th>
+                        <th>タグ</th>
+                    </tr>
+
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><a href="#">ラムダ式</a></td>
+                        <td>プログラミング</td>
+                        <td>Java,Java8,Stream</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">サーバーレスアーキテクチャ</a></td>
+                        <td>システム</td>
+                        <td>AWS,Lambda,APIGateway</td>
+                    </tr>
+                    <tr>
+                        <td><a href="contents001.html">マイクロサービスアーキテクチャ</a></td>
+                        <td>システム</td>
+                        <td>REST,MSA,SpringBoot</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">DIコンテナ</a></td>
+                        <td>フレームワーク</td>
+                        <td>SpringFramework,DI</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">GCアルゴリズム</a></td>
+                        <td>プログラミング</td>
+                        <td>GC,Java,G1GC,CSMC</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <a class="btn btn-info" href="#" role="button">More...</a>
+            </div>
+        </div>
+    </section>
+  </article>
+</main>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
